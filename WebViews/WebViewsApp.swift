8fddc93833de49805cf649cff2b8e33fe4d1ed61
Edwin <edwin12sv@gmail.com>
2023-12-29 00:13:59 -0600

    Initial Commit

---
+++ WebViews/WebViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewsApp.swift
+//  WebViews
+//
+//  Created by elsalvador on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WebViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
